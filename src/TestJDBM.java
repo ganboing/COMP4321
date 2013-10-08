a474fead4794edef462ae47468b92bdb8e47bb06
ganboing <ganboing@gmail.com>
2013-10-09 02:32:19 +0800

    mod

---
+++ src/TestJDBM.java
@@ -0,0 +1,50 @@
+public class TestJDBM {
+
+	public static void main(String[] args) throws Exception {
+		
+		for(String s : args)
+		{
+			System.out.println(s);
+		}
+		
+		if(args.length == 1)
+		{}
+
+		jdbm.RecordManager record_manager;
+		jdbm.htree.HTree hashtable;
+
+		try {
+			record_manager = jdbm.RecordManagerFactory
+					.createRecordManager("./test_db");
+		} catch (Exception e) {
+			System.out.println(e.toString());
+			return;
+		}
+
+		long recid;
+		recid = record_manager.getNamedObject("test_ht");
+		if (recid != 0) // If the object has already been recorded in record
+			// manager;
+			hashtable = jdbm.htree.HTree.load(record_manager, recid);
+
+		else // If not, create a new hashtable;
+		{
+			hashtable = jdbm.htree.HTree.createInstance(record_manager);
+			record_manager.setNamedObject("test_ht", hashtable.getRecid()); // Store
+																			// object
+			// hashtable ht1
+			// into recman
+			// as the name
+			// "ht1";
+		}
+
+		WebPageDescriptor web1 = new WebPageDescriptor(0x12345678, 0x12345678,
+				0xaabbccdd, new String("t"));
+
+		hashtable.put(((long) 0xabcd), web1);
+
+		record_manager.commit();
+		record_manager.close();
+	}
+
+}
--- src/WebPageDescriptor.java
+++ src/WebPageDescriptor.java
@@ -1,4 +1,18 @@
 
 public class WebPageDescriptor {
 
+	long interalId;
+	long collectedTime;
+	long lastModifiedTime;
+	
+	String URL;
+	
+	//XXX: only for testing
+	public WebPageDescriptor(long id, long collectedtime, long lastmodtime, String url)
+	{
+		this.interalId = id;
+		this.collectedTime = collectedtime;
+		this.lastModifiedTime = lastmodtime;
+		this.URL = new String(url);
+	}
 }
