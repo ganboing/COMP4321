cc29a6a9a2cad25ef2c101b38b12cd793192994d
ganboing <ganboing@gmail.com>
2013-10-09 18:20:57 +0800

    mod

--- src/WebPageDescriptor.java
+++ src/WebPageDescriptor.java
@@ -1,6 +1,13 @@
+import java.io.Serializable;
 
-public class WebPageDescriptor {
 
+public class WebPageDescriptor implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
 	long interalId;
 	long collectedTime;
 	long lastModifiedTime;
