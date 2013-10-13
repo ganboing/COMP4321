b82739717ce3e49d7ef226aa1d51fb899c0ff1a3
ganboing <ganboing@gmail.com>
2013-10-13 14:22:48 +0800

    mod

--- src/TestHttpConnection.java
+++ src/TestHttpConnection.java
@@ -11,7 +11,8 @@ public class TestHttpConnection {
 			System.out.print("url == ");
 			System.out.print(connection.getURL());
 			System.out.print('\n');
-			System.out.printf("last modified == %d \n", connection.getLastModified());
+			System.out.printf("last modified == ");
+			System.out.print(connection.getLastModified());
 		} catch (org.htmlparser.util.ParserException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
