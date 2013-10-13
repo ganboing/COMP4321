5c3d13f1762bc7308a42b8108428e6012c51a8f2
ganboing <ganboing@gmail.com>
2013-10-13 14:17:16 +0800

    mod

--- src/TestHttpConnection.java
+++ src/TestHttpConnection.java
@@ -3,8 +3,9 @@
 public class TestHttpConnection {
 	public static void main (String[] args)
     {
+		System.out.printf("argc == %d \n", args.length);
 		assert(args.length == 1);
-		String url_requested = args[1];
+		String url_requested = args[0];
 		try {
 			java.net.URLConnection connection = org.htmlparser.lexer.Page.getConnectionManager().openConnection(url_requested);
 			System.out.print("url == ");
