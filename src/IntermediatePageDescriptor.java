dfcbb7dfa79ad9d2cb66b54f0b1c44c647e4963b
ganboing <ganboing@gmail.com>
2013-10-16 17:21:40 +0800

    mod

--- src/IntermediatePageDescriptor.java
+++ src/IntermediatePageDescriptor.java
@@ -1,8 +1,14 @@
 
 public class IntermediatePageDescriptor {
 
-	public IntermediatePageDescriptor(String _s, String _w, java.net.URL[] _u)
+	String url_fetched;
+	String content;
+	java.net.URL[] links;
+	
+	public IntermediatePageDescriptor(String _url, String _content, java.net.URL[] _links)
 	{
-		
+		url_fetched = _url;
+		content = _content;
+		links = _links;	
 	}
 }
--- src/TestHttpConnection.java
+++ src/TestHttpConnection.java
@@ -1,4 +1,3 @@
-
 final class JavaDontHaveGlobalVar {
 	static final String[] urls = { "http://www.cse.ust.hk/",
 			"http://www.cse.ust.hk?lang=hk", "http://www.cse.ust.hk?lang=cn",
@@ -29,127 +28,122 @@ final class JavaDontHaveGlobalVar {
 			"http://www.cse.ust.hk/pg/",
 			"http://www.cse.ust.hk/pg/hkust_only/", };
 	int var_cnt = urls.length;
-	public synchronized String[] GiveMeUrl(int cnt)
-	{
-		if(var_cnt < cnt)
-		{
+
+	public synchronized String[] GiveMeUrl(int cnt) {
+		if (var_cnt < cnt) {
 			cnt = var_cnt;
 		}
-		return java.util.Arrays.copyOfRange(urls, urls.length - var_cnt, urls.length - var_cnt + cnt);
+		return java.util.Arrays.copyOfRange(urls, urls.length - var_cnt,
+				urls.length - var_cnt + cnt);
 	}
 }
 
-final class HttpWorkerThread implements java.util.concurrent.Callable<IntermediatePageDescriptor>
-{
+final class HttpWorkerTask implements
+		java.util.concurrent.Callable<IntermediatePageDescriptor> {
 
 	String url_to_fetch;
-	
-	public HttpWorkerThread(String _url_to_fetch)
-	{
+
+	public HttpWorkerTask(String _url_to_fetch) {
 		url_to_fetch = _url_to_fetch;
 	}
-	
+
 	@Override
-	public IntermediatePageDescriptor call() throws Exception 
-	{
-		org.htmlparser.beans.StringBean sb = new org.htmlparser.beans.StringBean();
-		sb.setURL(url_to_fetch);
-		String words = sb.getStrings();
-		org.htmlparser.beans.LinkBean lb = new org.htmlparser.beans.LinkBean();
-	    lb.setURL(url_to_fetch);
-	    java.net.URL[] URL_array = lb.getLinks();
-	    
-	    //XXX: add further processing
-	    
-		return new IntermediatePageDescriptor(url_to_fetch, words, URL_array);
+	public IntermediatePageDescriptor call() throws Exception {
+		System.out.printf("HttpWorker For %s is running\nk", url_to_fetch);
+		try {
+			org.htmlparser.beans.StringBean sb = new org.htmlparser.beans.StringBean();
+			sb.setURL(url_to_fetch);
+			String words = sb.getStrings();
+			org.htmlparser.beans.LinkBean lb = new org.htmlparser.beans.LinkBean();
+			lb.setURL(url_to_fetch);
+			java.net.URL[] URL_array = lb.getLinks();
+			return new IntermediatePageDescriptor(url_to_fetch, words,
+					URL_array);
+
+		} catch (Exception e) {
+			throw e;
+		} finally {
+			System.out.printf("HttpWorker For %s is finished\nk", url_to_fetch);
+		}
 	}
 }
 
-final class TheSoCalledMonitor implements Runnable{
-	
+final class TheSoCalledMonitor implements Runnable {
+
 	Object ThreadPool;
-	
-	TheSoCalledMonitor(Object _ThreadPool)
-	{
-		ThreadPool  = _ThreadPool;
+
+	TheSoCalledMonitor(Object _ThreadPool) {
+		ThreadPool = _ThreadPool;
 	}
-	
-	public void run()
-	{
-		
-		while(true)
-		{
+
+	public void run() {
+
+		while (true) {
 			try {
 				Thread.sleep(1000);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
+
 		}
 	}
 }
 
 public class TestHttpConnection {
-	public static void main (String[] args)
-    {
+	public static void main(String[] args) {
 		System.out.printf("argc == %d \n", args.length);
-		assert(args.length == 1);
-		String url_requested = args[0];
-		try {
-			//java.net.URLConnection connection = org.htmlparser.lexer.Page.getConnectionManager().openConnection(url_requested);
+		int page_cnt = Integer.parseInt(args[0]);
 
-		    //connection.setConnectTimeout(5000);
-		    //connection.setReadTimeout(10000);
-			
-		    org.htmlparser.beans.StringBean sb = new org.htmlparser.beans.StringBean();
-			sb.setURL(url_requested);
-			System.out.print(sb.getStrings());
-			
-		    org.htmlparser.beans.LinkBean lb = new org.htmlparser.beans.LinkBean();
-		    lb.setURL(url_requested);
-		    java.net.URL[] URL_array = lb.getLinks();
-		    for(int i=0; i<URL_array.length; i++){
-		    	System.out.println(URL_array[i]);
-		    }
-			/*System.out.print("url == ");
-			System.out.print(connection.getURL());
-			System.out.print('\n');
-			BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-			String inputLine;
-	        while ((inputLine = in.readLine()) != null) 
-	            System.out.println(inputLine);
-	        in.close();
-			System.out.print('\n');
-			System.out.printf("last modified == ");
-			System.out.print(connection.getLastModified());*/
-			/*try {
-				connection.connect();
-				System.out.print(connection.getContentType());
-				System.out.print('\n');
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-				System.out.print(e.toString());
-				System.out.print('\n');
+		java.util.concurrent.ExecutorService IdxExecutor = java.util.concurrent.Executors.newFixedThreadPool(0x04);
+		java.util.concurrent.CompletionService<IntermediatePageDescriptor> IdxExecSrv = new java.util.concurrent.ExecutorCompletionService<IntermediatePageDescriptor>(
+				IdxExecutor);
+
+		for (int i = 0; i < page_cnt; i++) {
+			IdxExecSrv.submit(new HttpWorkerTask(JavaDontHaveGlobalVar.urls[i]));
+		}
+
+		
+		
+		try {
+			while(true)
+			{
+				Thread.sleep(50);
 			}
-			try {
-				System.out.print(connection.getContent().toString());
-				System.out.print('\n');
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			System.out.print('\n');
-			System.out.printf("last modified == ");
-			System.out.print(connection.getLastModified());*/
-		    while(true);
+			// java.net.URLConnection connection =
+			// org.htmlparser.lexer.Page.getConnectionManager().openConnection(url_requested);
+
+			// connection.setConnectTimeout(5000);
+			// connection.setReadTimeout(10000);
+
+			/*
+			 * System.out.print("url == ");
+			 * System.out.print(connection.getURL()); System.out.print('\n');
+			 * BufferedReader in = new BufferedReader(new
+			 * InputStreamReader(connection.getInputStream())); String
+			 * inputLine; while ((inputLine = in.readLine()) != null)
+			 * System.out.println(inputLine); in.close();
+			 * System.out.print('\n'); System.out.printf("last modified == ");
+			 * System.out.print(connection.getLastModified());
+			 */
+			/*
+			 * try { connection.connect();
+			 * System.out.print(connection.getContentType());
+			 * System.out.print('\n'); } catch (IOException e) { // TODO
+			 * Auto-generated catch block e.printStackTrace();
+			 * System.out.print(e.toString()); System.out.print('\n'); } try {
+			 * System.out.print(connection.getContent().toString());
+			 * System.out.print('\n'); } catch (IOException e) { // TODO
+			 * Auto-generated catch block e.printStackTrace(); }
+			 * System.out.print('\n'); System.out.printf("last modified == ");
+			 * System.out.print(connection.getLastModified());
+			 */
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			System.out.print("catched in main\n");
 			e.printStackTrace();
 			System.out.print(e.toString());
 		}
-		
-    }
+
+	}
 }
