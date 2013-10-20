9b16519f6166254dc4c876ea057f0f72b5ec8e6a
ganboing <ganboing@gmail.com>
2013-10-20 23:12:56 +0800

    mod

--- src/IntermediatePageDescriptor.java
+++ src/IntermediatePageDescriptor.java
@@ -1,27 +1,27 @@
 public class IntermediatePageDescriptor {
-		
+
 	String url_fetched;
 	String content;
 	java.net.URL[] links;
-	
-	public IntermediatePageDescriptor(String _url, String _content, java.net.URL[] _links)
-	{
+
+	public IntermediatePageDescriptor(String _url, String _content,
+			java.net.URL[] _links) {
 		KeyWordMap keyword_map = new KeyWordMap();
 		java.util.Scanner scanner = new java.util.Scanner(_content);
-		
+
 		try {
 			while (true) {
 				String word = scanner.next();
 				keyword_map.addWord(word);
 			}
 		} catch (Exception e) {
-			assert(e instanceof java.util.NoSuchElementException);
-		}b
-		
+			assert (e instanceof java.util.NoSuchElementException);
+		}
+
 		scanner.close();
-		
+
 		url_fetched = _url;
 		content = _content;
-		links = _links;	
+		links = _links;
 	}
 }
