b0269947ff2ddb98acadc6ff917ac37cd10ec627
ganboing <ganboing@gmail.com>
2013-11-20 23:07:26 +0800

    mod

--- src/Query.java
+++ src/Query.java
@@ -1,16 +1,50 @@
-import java.util.LinkedList;
-
 public class Query {
-	private static java.util.regex.Pattern pattern_word = java.util.regex.Pattern
-			.compile("[a-zA-Z]+");
-	private static java.util.regex.Pattern pattern_phase = java.util.regex.Pattern
-			.compile("\"[^\"]*\"");
+	public java.util.List<Integer> query(String query_term) {
+		java.util.Map<String, Integer> keyword_weight_map = new java.util.HashMap<String, Integer>();
+		java.util.Map<String, Integer> keyphase_weight_map = new java.util.HashMap<String, Integer>();
 
-	public java.util.List<Integer> query (String query_term)
-	{
-		java.util.Map<Integer, Integer> a;
-		//a.
-		java.util.List<Integer> key_words = new java.util.LinkedList<Integer>();
-		
+		java.util.regex.Matcher phase_matcher = StringProc
+				.GetPhaseMatcher(query_term);
+
+		int phase_end_pos = 0;
+
+		while (phase_matcher.find()) {
+			String previous_keywords = query_term.substring(phase_end_pos,
+					phase_matcher.start());
+			java.util.regex.Matcher keyword_matcher = StringProc
+					.GetWordMatcher(previous_keywords);
+			while (keyword_matcher.find()) {
+				String keyword = keyword_matcher.group();
+				Integer weight = keyword_weight_map.get(keyword);
+				if (weight == null) {
+					keyword_weight_map.put(keyword, 1);
+				} else {
+					keyword_weight_map.put(keyword, weight + 1);
+				}
+			}
+			phase_end_pos = phase_matcher.end();
+			String phase = phase_matcher.group();
+			Integer weight = keyphase_weight_map.get(phase);
+			if (weight == null) {
+				keyphase_weight_map.put(phase, 1);
+			} else {
+
+				keyphase_weight_map.put(phase, weight + 1);
+			}
+		}
+		String next_keywords = query_term.substring(phase_end_pos);
+		java.util.regex.Matcher keyword_matcher = StringProc
+				.GetWordMatcher(next_keywords);
+		while (keyword_matcher.find()) {
+			String keyword = keyword_matcher.group();
+			Integer weight = keyword_weight_map.get(keyword);
+			if (weight == null) {
+				keyword_weight_map.put(keyword, 1);
+			} else {
+				keyword_weight_map.put(keyword, weight + 1);
+			}
+		}
+		return InvertedIdx.Query(keyword_weight_map, keyphase_weight_map);
+
 	}
 }
--- src/StringProc.java
+++ src/StringProc.java
@@ -1,16 +1,23 @@
 
 public class StringProc {
-	
-	private static java.util.regex.Pattern pattern_alpha = java.util.regex.Pattern
+
+	private static java.util.regex.Pattern pattern_word = java.util.regex.Pattern
 			.compile("[a-zA-Z]+");
-	
+	private static java.util.regex.Pattern pattern_phase = java.util.regex.Pattern
+			.compile("\"[^\"]*\"");
+
 	public static String Stem(String raw_word)
 	{
 		return Porter.Porter.stripAffixes(raw_word);
 	}
 	
-	public static java.util.regex.Matcher GetWordMatcher(String phase)
+	public static java.util.regex.Matcher GetWordMatcher(String term)
 	{
-		return pattern_alpha.matcher(phase);
+		return pattern_word.matcher(term);
+	}
+	
+	public static java.util.regex.Matcher GetPhaseMatcher(String phase)
+	{
+		return pattern_phase.matcher(phase);
 	}
 }
