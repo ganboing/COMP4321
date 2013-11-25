cdaef6917985354a979aadedb65dc3558b4dc4fc
ganboing <ganboing@gmail.com>
2013-11-26 02:40:15 +0800

    mod

--- src/comp4321_proj/QueryClient.java
+++ src/comp4321_proj/QueryClient.java
@@ -2,4 +2,16 @@ package comp4321_proj;
 
 public class QueryClient {
 
+	public static void main(String[] args) throws Exception {
+		String rmi_server = args[0];
+		QueryRMIInterface query_srv = (QueryRMIInterface) java.rmi.Naming
+				.lookup(rmi_server);
+		java.util.Scanner input_scan = new java.util.Scanner(System.in);
+		String query = input_scan.nextLine();
+		java.util.List<QueryResultEle> ret = query_srv.Query(query);
+		for (QueryResultEle e : ret) {
+			e.print();
+		}
+		input_scan.close();
+	}
 }
